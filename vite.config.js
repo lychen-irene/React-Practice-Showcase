bcfc6b40708d319df0d4c5d4d3dca271a4178152
Liang <chenliangyu09@gmail.com>
2026-01-18 00:26:43 +0800

    refactor: add PROJECT_PATH into .env and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base:
-    process.env.NODE_ENV === "production" ? "React-Practice-Showcase/" : "/",
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the
+  // `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    base: env.VITE_PROJECT_PATH,
+    plugins: [react()],
+  }
 });
