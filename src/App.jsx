2760deac24bd80819a058f2e8d7a5661d4f3d180
Liang <chenliangyu09@gmail.com>
2026-01-18 20:24:57 +0800

    chore: import eslint style config

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -20,18 +20,24 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button className="btn btn-primary" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button className="btn btn-primary" onClick={() => setCount(count => count + 1)}>
+          count is
+          {' '}
+          {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit
+          {' '}
+          <code>src/App.jsx</code>
+          {' '}
+          and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
