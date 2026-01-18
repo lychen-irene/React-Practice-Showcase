d8a6060e4fd7b6336f8dfc727762625f2060735c
Liang <chenliangyu09@gmail.com>
2026-01-18 20:24:57 +0800

    chore: import eslint style config

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,11 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig([
   globalIgnores(['dist']),
+  stylistic.configs.recommended,
   {
     files: ['**/*.{js,jsx}'],
     extends: [
@@ -15,12 +17,15 @@ export default defineConfig([
     ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: globals.browser, // load global variables that exist in browser environments
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        sourceType: 'module', // Enable module syntax and imply strict mode
       },
+    },
+    plugins: {
+      '@stylistic': stylistic,
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
