87a0a906bea47e068f12b6bb3b90345271b6ecd8
Liang <chenliangyu09@gmail.com>
2026-01-19 02:01:27 +0800

    fix: project-1 path 404 error

--- src/components/ProjectOnePage.jsx
+++ src/components/ProjectOnePage.jsx
@@ -1,3 +1,4 @@
+// import { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import projectOneUrl from '../components/projectOne.html?url'
 
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
