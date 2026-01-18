fe4277b31395eed0269f07962e1eb8b62f269abc
Liang <chenliangyu09@gmail.com>
2026-01-18 17:52:59 +0800

    refactor: rearrange css style config 1. add /style folder under /asset folder 2. edit all.scss, add _variables.scss and _variables-dark.scss to keep code clean by import 3. add _customize.scss to customize variables 4. add _loading.scss to customize style detail

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button className="btn" onClick={() => setCount((count) => count + 1)}>
+        <button className="btn btn-primary" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
