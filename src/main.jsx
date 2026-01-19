92354c8af60eecfd6ef765ae980ebea44d265f12
Liang <chenliangyu09@gmail.com>
2026-01-19 19:31:37 +0800

    style: change footer background color

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,8 @@ import App from './App.jsx'
 import ProjectOnePage from './components/ProjectOnePage.jsx'
 
 // import './index.css'
-import 'bootstrap'
+import 'bootstrap' // loads Bootstrap's JavaScript plugins
 import './assets/all.scss'
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
