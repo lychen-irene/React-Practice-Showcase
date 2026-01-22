0ea1bd03ade98fc1a4736c831d847cabc9e8f944
Liang <chenliangyu09@gmail.com>
2026-01-22 23:53:16 +0800

    refactor: edit description text for readability

--- src/components/ProjectTwoPage.jsx
+++ src/components/ProjectTwoPage.jsx
@@ -224,6 +224,8 @@ const ProjectTwoPage = () => {
                     <p>
                       以下產品資料來源為
                       {/* rel="noopener noreferrer" for prevent phishing */}
+                      {' '}
+                      {/* space within text */}
                       <a href="https://www.stonexp.idv.tw/i.h?cls=40&pg=0,1" target="_blank" rel="noopener noreferrer">石探紀：茶包的礦物化石網站</a>
                       <br />
                       此列表僅供作業練習與面試使用，非商業性質用途
